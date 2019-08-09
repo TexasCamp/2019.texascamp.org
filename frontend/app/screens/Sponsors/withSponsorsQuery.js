919b185706a4cd0e119b1ad5976fa1ff5caf5a9b
Brandon Williams <rocketeerbkw@gmail.com>
2019-08-09 04:01:26 -0500

    Revert "Add placeholder platinum sponsor if none are existing"
    This reverts commit 1aebd1bacb5a6d2fd649fe349c933f461cce9333.

--- frontend/app/screens/Sponsors/withSponsorsQuery.js
+++ frontend/app/screens/Sponsors/withSponsorsQuery.js
@@ -37,27 +37,8 @@ export const SPONSOR_QUERY = gql`
   }
 `;
 
-const emptySponsor = {
-  id: 'empty-sponsor',
-  title: 'We Need You!',
-  body: 'We Need You!',
-  sponsorLevel: 'Platinum',
-  sponsorWeight: 0,
-  image: {
-    url: 'http://placekitten.com/421/240',
-    alt: 'We Need You!',
-  },
-  sponsorUrl: '/sponsors',
-};
-
-export const sponsorsListMapper = (
-  entities: Array<Object>,
-): Array<SponsorT> => {
-  if (entities.length < 1) {
-    return [emptySponsor];
-  }
-
-  return entities.map(entity => ({
+export const sponsorsListMapper = (entities: Array<Object>): Array<SponsorT> =>
+  entities.map(entity => ({
     id: entity.entityId,
     title: entity.title,
     body: entity.body.value,
@@ -69,7 +50,6 @@ export const sponsorsListMapper = (
     },
     sponsorUrl: path(['fieldSponsorUrl', 'uri'], entity),
   }));
-};
 
 const withSponsorsQuery = graphql(SPONSOR_QUERY, {
   props: ({
